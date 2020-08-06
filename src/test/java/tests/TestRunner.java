6c0c53b14678916e510b92208ae3c9684993548b
Stanislav Vdoviuk <stanislav.vdoviuk@gmail.com>
2020-08-06 15:16:48 +0300

    Added logs, removed chrome driver exe

--- src/test/java/tests/TestRunner.java
+++ src/test/java/tests/TestRunner.java
@@ -1,42 +1,53 @@
 package tests;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterClass;
-
-import java.nio.file.Paths;
 import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class TestRunner {
     private String url = "http://dev.pyoor.co/";
     private static WebDriver driver;
+    private static final Logger LOGGER = Logger.getLogger( TestRunner.class.getName() );
 
     protected WebDriver launch() throws InterruptedException {
+        LOGGER.info( "TestRunner.launch() starts");
         WebDriver driver = getDriver();
+        LOGGER.info("TestRunner.launch() running get(" + url + ")");
         driver.get(url);
-        if (System.getenv("JENKINS_HOME") != null) {
-            System.setProperty("webdriver.gecko.driver", Paths.get("").toAbsolutePath().toString() + "\\DriverFile\\chromedriver_win32\\chromedriver.exe");
-        }
-
-        driver.manage().window().setSize(new Dimension(1024, 768));
+        LOGGER.info("TestRunner.launch() running window().maximize()");
         driver.manage().window().maximize();
+        LOGGER.info("TestRunner.launch() .timeouts().implicitlyWait()");
         driver.manage().timeouts().implicitlyWait(1000, TimeUnit.SECONDS);
+
+        LOGGER.info("TestRunner.launch() finished");
         return driver;
     }
     protected WebDriver getDriver() throws InterruptedException {
+        LOGGER.info("TestRunner.getDriver() starts");
         if (driver == null) {
+            LOGGER.info("TestRunner.getDriver() driver is null");
+
+            LOGGER.info("TestRunner.getDriver() chromedriver().setup() running");
             WebDriverManager.chromedriver().setup();
+
+            LOGGER.info("TestRunner.getDriver() creating ChromeDriver()");
             driver = new ChromeDriver();
             launch();
             Thread.sleep(2000);
         }
+
+        LOGGER.info("TestRunner.getDriver() finished");
         return driver;
     }
     @AfterClass(alwaysRun = true)
     public void afterClass() {
+        LOGGER.info("TestRunner.afterClass() starts");
         driver.quit();
         driver = null;
+        LOGGER.info("TestRunner.afterClass() finished");
     }
 }
